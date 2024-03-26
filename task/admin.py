a5fc5ad4ba31431a7d1dfda7ced8949470bb9ed9
max <cyan0908@163.com>
2024-03-26 14:16:08 +0800

    feat: add django admin front

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class AlgorithmProcessQueueInline(admin.StackedInline):
+    model = models.AlgorithmProcessQueue
+    extra = 0
+
+class AlgorithmTaskAdmin(admin.ModelAdmin):
+    inlines = [AlgorithmProcessQueueInline]
+
+
+admin.site.register(models.AlgorithmSystemRuntimeControl)
+admin.site.register(models.AlgorithmSystemRuntimeStatus)
+admin.site.register(models.AlgorithmUserRuntimeStatus)
+admin.site.register(models.AlgorithmTask, AlgorithmTaskAdmin)
